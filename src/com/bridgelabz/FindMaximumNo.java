205b6b81b802b971c77e75972692653efea2c4c6
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-08-24 23:28:26 +0530

    ADDED:find the String maximum

--- src/com/bridgelabz/FindMaximumNo.java
+++ src/com/bridgelabz/FindMaximumNo.java
@@ -18,10 +18,18 @@ public class FindMaximumNo {
             maximum = z;
         return maximum;
     }
+    public String MaxNo(String x, String y, String z) {
+        String maximum = x;
+        if (y.compareTo(maximum) > 0)
+            maximum = y;
+        if (z.compareTo(maximum) > 0)
+            maximum = z;
+        return maximum;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to find maximum no");
         FindMaximumNo maximum = new FindMaximumNo();
-       // System.out.println(max.MaxNo(50,80,70));
-        System.out.println(maximum.FloatMaxNo(1.5f,3.6f, 2.6f));
+        System.out.println(maximum.MaxNo("Apple","Peach","Banana"));
+        //System.out.println(maximum.FloatMaxNo(1.5f,3.6f, 2.6f));
     }
 }
