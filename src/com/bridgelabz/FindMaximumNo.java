e6008aad63df53b5e0f0af8c1af0aaf175ff769a
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-08-25 07:38:46 +0530

    Refactor: Creat a Generic Class take 3 variable of generic type

--- src/com/bridgelabz/FindMaximumNo.java
+++ src/com/bridgelabz/FindMaximumNo.java
@@ -1,20 +1,30 @@
 package com.bridgelabz;
 
-public class FindMaximumNo {
-    public <T extends Comparable> T MaxNo(T x, T y, T z){
+public class FindMaximumNo<T extends  Comparable> {
+    public T X, Y, Z;
+    public FindMaximumNo(T x, T y, T z) {
+         X = x;
+         Y = y;
+         Z = z;
+    }
+
+    public static  <T extends Comparable> T MaxNo(T x, T y, T z){
         T maximum = x;
         if(y.compareTo(maximum) > 0)
         maximum = y;
         if(z.compareTo(maximum) >0)
         maximum = z;
+        System.out.println("The maximum is " +maximum);
         return  maximum;
+    }
+    public  void maiximumIs (){
+        FindMaximumNo.MaxNo(X, Y, Z);
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to find maximum no");
-        FindMaximumNo maximum = new FindMaximumNo();
-        System.out.println("The maximum is " +maximum.MaxNo(50,60, 70));
-        System.out.println("The maximum is " +maximum.MaxNo(1.5f,3.6f, 2.6f));
-        System.out.println("The maximum is " +maximum.MaxNo("Apple","Peach","Banana"));
+        new FindMaximumNo<Integer>(50, 70, 60).maiximumIs();
+        new FindMaximumNo<Float>(1.2f, 3.4f, 2.6f).maiximumIs();
+        new FindMaximumNo<String>("Apple", "Peach", "Banana").maiximumIs();
     }
 }
